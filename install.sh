2e5e01a43f44a2aa269f6947647098fff04fbfd7
WrenHavoc <wrenhavoc@proton.me>
2025-02-14 19:40:09 -0700

    okokokok, I think this should do it. Thanks again Ian, you're a great lab rat!

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ curl -fsSL https://ollama.com/install.sh | sh
 
 # Install python and Open WebUI
 sudo apt install python3 -y
-sudo apt install python3.11-venv -y
+sudo apt install python3.12-venv -y
 mkdir ~/open-webui-venv
 python3 -m venv ~/open-webui-venv
-~/open-webui-venv/bin/python pip install open-webui
+~/open-webui-venv/bin/pip install open-webui
 
 # copy files into bin
 sudo cp localai /bin
